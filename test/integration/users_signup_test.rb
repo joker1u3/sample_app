14e02728b9e9b690e9bf1f2785aa9f7ef73f083e
wangjiakun <wjkjyp@gmail.com>
2015-04-23 02:26:53 +0800

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
   
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,25 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+
+  # 如果用户已经登录，返回 true
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def log_in_as(user, options = {})
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path,session: { email:       user.email,
+                                 password:    password,
+                                 remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+  private
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
