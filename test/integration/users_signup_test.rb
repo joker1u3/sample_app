e2b7421e8ee423dd661667d8d130eb147571dfb2
wangjiakun <wjkjyp@gmail.com>
2015-04-22 15:42:09 +0800

    Finish chapter 7 exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
   test "valid signup information" do
@@ -21,6 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                              password_confirmation: "showme" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
   
 
